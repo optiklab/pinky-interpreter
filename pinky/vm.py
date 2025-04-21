f4b4489a68168a7afadeeb986be44ba5ea61b3cb
Anton Yarkov <anton.yarkov@gmail.com>
2025-04-21 19:31:55 +0200

    Global VM Memory Load & Store

--- pinky/vm.py
+++ pinky/vm.py
@@ -42,10 +42,10 @@
 #
 # Instructions to load and store variables
 #
-#      ('LOAD', name)        # Push a global variable name from memory to the stack
-#      ('STORE, name)        # Save top of the stack into global variable by name
-#      ('LOAD_LOCAL', name)  # Push a local variable name from memory to the stack
-#      ('STORE_LOCAL, name)  # Save top of the stack to local variable by name
+#      ('LOAD_GLOBAL', name) # Push a global variable name from memory to the stack
+#      ('STORE_GLOBAL, name) # Save top of the stack into global variable by name
+#      ('LOAD_LOCAL', slot)  # Push a local variable from a stack slot/index to the top of the stack
+#      ('STORE_LOCAL, slot)  # Save top of the stack to local variable by slot/index 
 #
 # Instructions to manage control-flow (if-else, while, etc.)
 #
@@ -64,6 +64,7 @@ class VM:
   def __init__(self):
     self.stack = []
     self.labels = {}
+    self.globals = {}
     self.pc = 0
     self.sp = 0
     self.is_running = False
@@ -103,7 +104,7 @@ class VM:
     lefttype, leftval = self.POP()
     if lefttype == TYPE_NUMBER and righttype == TYPE_NUMBER:
       self.PUSH((TYPE_NUMBER, leftval + rightval))
-    if lefttype == TYPE_STRING or righttype == TYPE_STRING:
+    elif lefttype == TYPE_STRING or righttype == TYPE_STRING:
       self.PUSH((TYPE_STRING, stringify(leftval) + stringify(rightval)))
     else:
       vm_error(f'Error on ADD between {lefttype} and {righttype}.', self.pc - 1)
@@ -267,6 +268,12 @@ class VM:
     valtype, val = self.POP()
     if val == 0 or val == False:
       self.pc = self.labels[label]
+
+  def STORE_GLOBAL(self, name):
+    self.globals[name] = self.POP()
+
+  def LOAD_GLOBAL(self, name):
+    self.PUSH(self.globals[name])
 
   def HALT(self):
     self.is_running = False
