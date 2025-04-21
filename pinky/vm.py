ca0b34538d3607f24b0c27c68055be43feebbde7
Anton Yarkov <anton.yarkov@gmail.com>
2025-04-21 19:45:15 +0200

    Return Statements

--- pinky/vm.py
+++ pinky/vm.py
@@ -277,11 +277,11 @@ class VM:
       self.pc = self.labels[label]
 
   def JSR(self, label):
-    numargstype, numargs = self.POP() # <-- pop the numargs (the last value that was pushed to the top of the stack before JSR)
+    _, numargs = self.POP()
     base_pointer = self.sp - numargs
     new_frame = Frame(name=label, ret_pc=self.pc, fp=base_pointer)
     self.frames.append(new_frame)
-    self.pc = self.labels[label] # <-- jump to the subroutine
+    self.pc = self.labels[label]
 
   def RTS(self):
     self.pc = self.frames[-1].ret_pc
